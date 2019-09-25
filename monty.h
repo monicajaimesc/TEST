5a6727b65ed3047f32f183e6c561bf4bd1756ba1
nataliamedinat <nataliamedinabotero@gmail.com>
2019-09-25 20:20:04 +0000

    change

--- monty.h
+++ monty.h
@@ -12,7 +12,7 @@
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
+ * @n: integer.
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
  *
